92ce41444fe31efe141c67e0bae0665e434f9eb1
mokeshi3 <mokeshi34649>
2020-09-20 10:01:46 +0900

    add sphere.rs

--- src/hit.rs
+++ src/hit.rs
@@ -1,12 +1,12 @@
 use crate::ray::Ray;
 use crate::vec3::{Point3, Vec3};
 
-struct HitRecord {
-    p: Point3,
-    normal: Vec3<f64>,
-    t: f64,
+pub struct HitRecord {
+    pub p: Point3,
+    pub normal: Vec3<f64>,
+    pub t: f64,
 }
 
-trait hittable {
-    fn hit(r: &Ray, t_min: f64, t_max: f64, rec: HitRecord) -> bool;
+pub trait Hittable {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
 }
---
+++ src/sphere.rs
@@ -0,0 +1,53 @@
+use crate::hit::{HitRecord, Hittable};
+use crate::ray::Ray;
+use crate::vec3::{Point3, dot};
+
+struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+#[allow(dead_code)]
+impl Sphere {
+    fn new() -> Self {
+        Self {
+            center: Point3::new(),
+            radius: 0.,
+        }
+    }
+
+    fn build(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, tmin: f64, tmax: f64, rec: &mut HitRecord) -> bool {
+        let oc = *r.origin() - self.center;
+        let a = r.direction().length_squared();
+        let half_b = dot(&oc, r.direction());
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
+
+        if discriminant > 0. {
+            let root = discriminant.sqrt();
+            let temp = (-half_b - root) / a;
+            if temp < tmax && temp > tmin {
+                rec.t = temp;
+                rec.p = r.at(rec.t);
+                rec.normal = (rec.p - self.center) / self.radius;
+                return true;
+            }
+
+            let temp = (-half_b + root) / a;
+            if temp < tmax && temp > tmin {
+                rec.t = temp;
+                rec.p = r.at(rec.t);
+                rec.normal = (rec.p - self.center) / self.radius;
+                return true;
+            }
+        }
+
+        false
+    }
+}
