83a402ba018f4c6849d06adf73f01a765f7ad499
mokeshi3 <mokeshi34649>
2020-09-23 21:01:01 +0900

    add rtweekend.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod ray;
 pub mod hit;
 pub mod sphere;
 pub mod hittable_list;
+pub mod rtweekend;
---
+++ src/rtweekend.rs
@@ -0,0 +1,10 @@
+#[allow(dead_code)]
+const INFINITY: f64 = f64::MAX;
+
+#[allow(dead_code)]
+const PI: f64 = 3.1415926535897932385;
+
+#[allow(dead_code)]
+fn degrees_to_radians(degrees: f64) -> f64 {
+    degrees * PI / 180.0
+}
